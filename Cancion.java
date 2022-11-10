8c8b85642a2a8a9a7c8856d56d32d064ac4a4103
ElrohirGT <elrohirgt@gmail.com>
2022-11-10 10:04:11 -0600

    Modified constructors and provided a toString implementation

--- Cancion.java
+++ Cancion.java
@@ -1,33 +1,36 @@
 import java.time.Duration;
-public class Cancion implements ICancion{
+
+public class Cancion implements ICancion {
     private String _nombreCancion;
     private Duration _duracionCancion;
     private String _generoCancion;
     private String _autorCancion;
 
-
-
-    public Cancion(String _nombreCancion, Duration _duracionCancion, String _generoCancion, String _autorCancion) {
-        this._nombreCancion = _nombreCancion;
-        this._duracionCancion = _duracionCancion;
-        this._generoCancion = _generoCancion;
-        this._autorCancion = _autorCancion;
+    public Cancion(String nombreCancion, Duration duracionCancion, String generoCancion, String autorCancion) {
+        _nombreCancion = nombreCancion;
+        _duracionCancion = duracionCancion;
+        _generoCancion = generoCancion;
+        _autorCancion = autorCancion;
     }
 
-    public String obtenerNombre(){
+    public String obtenerNombre() {
         return _nombreCancion;
     }
 
-    public Duration obtenerDuracion(){
+    public Duration obtenerDuracion() {
         return _duracionCancion;
     }
 
-    public String obtenerGenero(){
+    public String obtenerGenero() {
         return _generoCancion;
     }
 
-    public String obtenerAutor(){
+    public String obtenerAutor() {
         return _autorCancion;
     }
 
+    @Override
+    public String toString() {
+        return _nombreCancion;
+    }
 }
